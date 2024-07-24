176fc335b5ce5491a236ba87edce3082fce27dd9
LyCaCa24 <datptpk03333@fpt.edu.vn>
2024-07-24 13:07:23 +0700

    d

---
+++ api_dong_ho/Migrations/20240724053505_Create_ef_dat.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_dong_ho.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_ef_dat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
