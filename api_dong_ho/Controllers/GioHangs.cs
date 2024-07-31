3e9400e1e0737570bbfcedd7f6df61748abf1cae
LyCaCa24 <datptpk03333@fpt.edu.vn>
2024-07-31 13:26:21 +0700

    i

--- api_dong_ho/Controllers/GioHangs.cs
+++ api_dong_ho/Controllers/GioHangs.cs
@@ -31,54 +31,53 @@ namespace api_dong_ho.Controllers
             var soLuong = request.SoLuong;
             var gioh = cart;
             var item = gioh.SingleOrDefault(p => p.MaSP == id);
-            var sanp = db.SanPham.Include(a => a.KichThuocs)
-                .Include(a => a.MauSacs).FirstOrDefaultAsync(sp => sp.MaSP == id);
+            var sanp = db.SanPham.FirstOrDefaultAsync(sp => sp.MaSP == id);
 
-            if (item == null)
-            {
-                if (sanp == null)
-                {
-                    return NotFound("Sản Phẩm Không Tồn Tại.");
-                }
-                else
-                {
+            //if (item == null)
+            //{
+            //    if (sanp == null)
+            //    {
+            //        return NotFound("Sản Phẩm Không Tồn Tại.");
+            //    }
+            //    else
+            //    {
 
 
-                    // Tạo đối tượng giohang với dữ liệu từ API
-                    item = new giohang
-                    {
-                        MaSP = sanp.Result.MaSP,
-                        TenSP = sanp.Result.TenSP,
-                        HinhAnh = sanp.Result.HinhAnhs.FirstOrDefault()?.TenHinhAnh ?? "",
-                        gia = sanp.Result.Gia,
-                        TenKT = sanp.Result.KichThuocs.FirstOrDefault(kt => kt.MaKichThuoc == request.maKT).TenKichThuoc,
-                        TenMS = sanp.Result.MauSacs.FirstOrDefault(kt => kt.MaMauSac == request.maMS).TenMauSac,
-                        SoLuong = soLuong
-                    };
+            //        // Tạo đối tượng giohang với dữ liệu từ API
+            //        item = new giohang
+            //        {
+            //            MaSP = sanp.Result.MaSP,
+            //            TenSP = sanp.Result.TenSP,
+            //            HinhAnh = sanp.Result.HinhAnhs.FirstOrDefault()?.TenHinhAnh ?? "",
+            //            gia = sanp.Result.Gia,
+            //            TenKT = sanp.Result.KichThuocs.FirstOrDefault(kt => kt.MaKichThuoc == request.maKT).TenKichThuoc,
+            //            TenMS = sanp.Result.MauSacs.FirstOrDefault(kt => kt.MaMauSac == request.maMS).TenMauSac,
+            //            SoLuong = soLuong
+            //        };
 
-                    gioh.Add(item);
-                }
-            }
-            else
-            {
-                item.SoLuong += soLuong;
-                var kichThuoc = sanp.Result.KichThuocs.FirstOrDefault(kt => kt.MaKichThuoc == request.maKT);
-                if (kichThuoc != null && !item.TenKT.Contains(kichThuoc.TenKichThuoc))
-                {
-                    item.TenKT = string.Join(", ", item.TenKT, kichThuoc.TenKichThuoc).Trim(new char[] { ',', ' ' });
-                }
+            //        gioh.Add(item);
+            //    }
+            //}
+            //else
+            //{
+            //    item.SoLuong += soLuong;
+            //    var kichThuoc = sanp.Result.KichThuocs.FirstOrDefault(kt => kt.MaKichThuoc == request.maKT);
+            //    if (kichThuoc != null && !item.TenKT.Contains(kichThuoc.TenKichThuoc))
+            //    {
+            //        item.TenKT = string.Join(", ", item.TenKT, kichThuoc.TenKichThuoc).Trim(new char[] { ',', ' ' });
+            //    }
 
-                // Cập nhật màu sắc
-                var mauSac = sanp.Result.MauSacs.FirstOrDefault(ms => ms.MaMauSac == request.maMS);
-                if (mauSac != null && !item.TenMS.Contains(mauSac.TenMauSac))
-                {
-                    item.TenMS = string.Join(", ", item.TenMS, mauSac.TenMauSac).Trim(new char[] { ',', ' ' });
-                }
-            }
+            //    // Cập nhật màu sắc
+            //    var mauSac = sanp.Result.MauSacs.FirstOrDefault(ms => ms.MaMauSac == request.maMS);
+            //    if (mauSac != null && !item.TenMS.Contains(mauSac.TenMauSac))
+            //    {
+            //        item.TenMS = string.Join(", ", item.TenMS, mauSac.TenMauSac).Trim(new char[] { ',', ' ' });
+            //    }
+            //}
 
-            HttpContext.Session.Set(MySetting.GioHang_KEY, gioh);
+            //HttpContext.Session.Set(MySetting.GioHang_KEY, gioh);
 
-            return Ok(cart);
+            return Ok(sanp);
         }
     }
 }
